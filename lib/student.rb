2620db37079393b198e78c1c15a27e2e1b9903d4
wachira <wachiraa954@gmail.com>
2022-08-27 13:45:34 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,9 @@
-class Student 
-
+require_relative './user'
+class Student <User
+   def learn(knowledge)
+    @knowledge<<"Ruby framework Rails gem bundle update"
+   end 
+   def knowledge
+    "Javascript Ember Elixir knowledge"
+   end 
 end
